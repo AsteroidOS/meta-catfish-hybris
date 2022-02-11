0674182652fff57d52e1c7357abb893e5c14e6d9
Cristian Le <git@lecris.me>
2022-02-11 18:55:29 +0900

    Change to use hybris_dlopen and fixed timer
    Signed-off-by: Cristian Le <git@lecris.me>

--- recipes-core/lcd-tools/lcd-tools/lcd-sync-time.cpp
+++ recipes-core/lcd-tools/lcd-tools/lcd-sync-time.cpp
@@ -2,25 +2,25 @@
 
 #include <iostream>
 #include <dlfcn.h>
-#include <hybris/common/binding.h>
+#include <hybris/common/dlfcn.h>
 
 int AsteroidOS::LCD_Tools::SyncTime() {
 	void* handler;
 	int (*syncTime)();
-	handler = android_dlopen("libmcutool.so", RTLD_LAZY);
+	handler = hybris_dlopen("libmcutool.so", RTLD_LAZY);
 	if (!handler) {
 		std::cerr << "Unable to load libmcutool.so" << std::endl;
 		return -1;
 	}
 	syncTime = (int (*)())
-			android_dlsym(handler, "Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeSyncTime");
+			hybris_dlsym(handler, "Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeSyncTime");
 	if (!syncTime){
 		std::cerr << "Unable to get symbol" << std::endl;
-		android_dlclose(handler);
+		hybris_dlclose(handler);
 		return -1;
 	}
 	auto res = syncTime();
-    if (android_dlclose(handler)){
+    if (hybris_dlclose(handler)){
 		std::cerr << "Failed to safely close the library" << std::endl;
 		return -1;
 	}
